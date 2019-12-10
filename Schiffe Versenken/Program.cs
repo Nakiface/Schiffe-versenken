5b2bc8c8ee2db0617768cfa173ab262b2b928227
Nakiface <BernhardSchmitt@googlemail.com>
2019-12-10 11:37:00 +0100

    Projektdateien hinzufügen.

---
+++ Schiffe Versenken/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Schiffe_Versenken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
